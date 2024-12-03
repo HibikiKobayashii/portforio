d570111e92d2c1ed1bf5b3f84d04eb63f15da8e4
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-03 17:36:32 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ function Home() {
     <div>
       <Header />
       <main style={{ padding: "20px" }}>
-        <h1>API実習ポートフォ</h1>
+        <h1>API実習ポートフォリオ</h1>
         <p>This is a basic Next.js site with a TypeScript header.</p>
       </main>
     </div>
