bb726e3eb278198f17dabebb96a6af48f90d7f42
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-05 18:08:21 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ function Home() {
             src="https://raw.githubusercontent.com/HibikiKobayashii/HibikiKobayashii.github.io/main/gazo/hibiki.jpg"
             alt="サンプル画像"
             caption="コントラスト調整された画像"
-            offsetX={450} // 横方向のオフセット
+            offsetX={400} // 横方向のオフセット
             offsetY={-170} // 縦方向のオフセット
             contrast={75} // コントラストを150%に設定
           />
