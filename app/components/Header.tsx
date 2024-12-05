4c72cdcb0bb6c9f48b251bd1b7c55609a61a4a31
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-05 13:08:22 +0900

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC = () => {
             </li>
             <li style={navItemStyle}>
               <a href="#" style={linkStyle}>
-                About
+                About me
               </a>
             </li>
             <li style={navItemStyle}>
