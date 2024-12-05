dc271f39bdd44f0e23ce3a3155a851ae509919e5
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-05 17:20:04 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ function Home() {
             alt="サンプル画像"
             caption="コントラスト調整された画像"
             offsetX={400} // 横方向のオフセット
-            offsetY={-300} // 縦方向のオフセット
+            offsetY={-100} // 縦方向のオフセット
             contrast={75} // コントラストを150%に設定
           />
         </section>
