49f3f76931ff31ebd9b0cc0b407ea5d814d8583b
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-06 15:39:01 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import Header from "../app/components/Header";
 import Gazo from "../app/components/gazo";
@@ -28,10 +26,8 @@ const HorizontalBar: React.FC<BarProps> = ({ width, height, color, offsetX, offs
 };
 
 function Home() {
-  
-
   return (
-    <div style={{ backgroundColor: "#aliceblue", minHeight: "100vh" }}>
+    <div style={{ backgroundColor: "#aliceblue", minHeight: "100vh", position: "relative" }}>
       <Header />
       <main style={{ padding: "20px" }}>
         <div
@@ -57,7 +53,7 @@ function Home() {
                 textAlign: "center",
                 position: "relative",
                 left: `${100}px`, // 横方向のオフセット
-                top: `${-60}px`, // 縦方向のオフセット
+                top: `${-60}px}`, // 縦方向のオフセット
               }}
             >
               I am an IoT Student !!
@@ -71,43 +67,48 @@ function Home() {
               offsetX={100} // 横方向のオフセット
               offsetY={-70} // 縦方向のオフセット
             />
-             <h2
+            <h2
               style={{
                 margin: 0,
                 fontSize: `${30}px`, // フォントサイズを動的に変更
                 textAlign: "center",
                 position: "relative",
                 left: `${100}px`, // 横方向のオフセット
-                top: `${-30}px`, // 縦方向のオフセット
+                top: `${-30}px}`, // 縦方向のオフセット
               }}
             >
-               <br></br>
-             開志専門職大学でIoTを学んでいる小林響と申します！
-            <br></br>
-            <br></br>
-         
-             XR系に力を入れています！
-         
-             
-
+              <br></br>
+              開志専門職大学でIoTを学んでいる小林響と申します！
+              <br></br>
+              <br></br>
+              XR系に力を入れています！
             </h2>
           </div>
+          
           {/* 画像 */}
           <Gazo
             src="https://raw.githubusercontent.com/HibikiKobayashii/HibikiKobayashii.github.io/main/gazo/hibiki.jpg"
             alt="小林響"
-       
             offsetX={270}
             offsetY={20}
             contrast={50}
           />
         </div>
 
-       
-           
-            
-          
-         
+        {/* 背景画像 */}
+        <div
+          style={{
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            backgroundImage: "url('https://raw.githubusercontent.com/HibikiKobayashii/HibikiKobayashii.github.io/main/gazo/hibiki.jpg')", // 背景画像のURL
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            zIndex: -1, // 他のコンテンツの後ろに配置
+          }}
+        ></div>
       </main>
     </div>
   );
