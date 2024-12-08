cb5ec1a2ba158229cb3cce4d4e41a0cef5d485b3
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-09 00:50:36 +0900

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -32,7 +32,7 @@ const Header: React.FC = () => {
 };
 
 const headerStyle: React.CSSProperties = {
-  backgroundColor: "#06131a",
+  backgroundColor: "#03090d",
   color: "white",
   padding: "10px 20px",
   position: "sticky",
