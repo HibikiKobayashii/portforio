6b00985d4ee9617945c37a0f5bb83937069785c9
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-08 20:34:32 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,67 @@
-"use client";
-
 import React from "react";
+import Header from "../app/components/Header";
+import Gazo from "../app/components/gazo";
 
-const Header: React.FC = () => {
+const HorizontalBar: React.FC<{ width: string; height: string; color: string }> = ({
+  width,
+  height,
+  color,
+}) => {
   return (
-    <header style={headerStyle}>
-      <div style={containerStyle}>
-        <h1 style={logoStyle}>ポートフォリオ</h1>
-        <nav>
-          <ul style={navListStyle}>
-            <li style={navItemStyle}>
-              <a href="#" style={linkStyle}>
-                Home
-              </a>
-            </li>
-            <li style={navItemStyle}>
-              <a href="#" style={linkStyle}>
-                Biography
-              </a>
-            </li>
-            <li style={navItemStyle}>
-              <a href="#" style={linkStyle}>
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+    <div
+      style={{
+        width,
+        height,
+        backgroundColor: color,
+        margin: "0 auto", // 棒を中央揃え
+      }}
+    ></div>
   );
 };
 
-const headerStyle: React.CSSProperties = {
-  backgroundColor: "#282c34",
-  color: "white",
-  padding: "10px 20px",
-  position: "sticky",
-  top: 0,
-  zIndex: 1000,
-  width: "100%", // ヘッダーを画面全幅に
-};
+function Home() {
+  return (
+    <div style={{ backgroundColor: "#aliceblue", minHeight: "100vh", position: "relative" }}>
+      <Header />
+      <main style={{ padding: "20px", textAlign: "center" }}>
+        <div>
+          <h1 style={{ fontSize: "calc(2rem + 1vw)", marginBottom: "10px" }}>
+            I am an IoT Student !!
+          </h1>
+          <HorizontalBar width="80%" height="5px" color="black" />
+          <h2 style={{ fontSize: "calc(1rem + 0.5vw)", marginTop: "20px" }}>
+            開志専門職大学でIoTを学んでいる小林響と申します！
+            <br />
+            XR系に力を入れています！
+          </h2>
+        </div>
+        <div style={{ marginTop: "20px" }}>
+          <Gazo
+            src="https://raw.githubusercontent.com/HibikiKobayashii/HibikiKobayashii.github.io/main/gazo/hibiki.jpg"
+            alt="小林響"
+            width={300}
+            height={400}
+            contrast={75}
+          />
+        </div>
+        {/* 背景画像 */}
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundImage:
+              "url('https://raw.githubusercontent.com/HibikiKobayashii/HibikiKobayashii.github.io/main/gazo/1.jpg')",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            zIndex: -1,
+          }}
+        ></div>
+      </main>
+    </div>
+  );
+}
 
-const containerStyle: React.CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  margin: "0 auto", // 要素を中央揃え
-  maxWidth: "1200px", // コンテンツの最大幅を指定
-};
-
-const logoStyle: React.CSSProperties = {
-  fontSize: "1.5rem",
-  margin: 0,
-};
-
-const navListStyle: React.CSSProperties = {
-  listStyle: "none",
-  display: "flex",
-  gap: "15px",
-  margin: 0,
-  padding: 0,
-};
-
-const navItemStyle: React.CSSProperties = {
-  display: "inline",
-};
-
-const linkStyle: React.CSSProperties = {
-  color: "white",
-  textDecoration: "none",
-  fontSize: "1rem",
-  transition: "color 0.3s ease",
-};
-
-export default Header;
+export default Home;
