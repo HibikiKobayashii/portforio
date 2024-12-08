b521299a8a68f18e8d5ca7f6d1bad746d667c4b3
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-09 01:15:40 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,161 +1,212 @@
-{/* スキルセクション */}
-<section
-  id="skills"
-  style={{
-    marginTop: "50px",
-    padding: "20px",
-    backgroundColor: "#03090d",
-    borderRadius: "10px",
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    display: "flex",
-    justifyContent: "space-between",
-  }}
->
-  <div style={{ marginBottom: "20px" }}>
-    {/* スキルセクションの上に追加する文字 */}
-    <h2 style={{ color: "#7ebeab", textAlign: "center", marginBottom: "10px" }}>
-      私のスキル
-    </h2>
-    <p style={{ color: "#7ebeab", textAlign: "center" }}>
-      ここでは、私が習得している技術を紹介します。
-    </p>
-  </div>
+import React from "react";
+import Header from "../app/components/Header"; // エイリアスを使ってHeaderをインポート
+import TextContent from "../app/components/TextContent"; // エイリアスを使ってTextContentをインポート
+import SkillDescription from "../app/components/SkillDescription"; // エイリアスを使ってSkillDescriptionをインポート
 
-  <div style={{ flex: 1, paddingRight: "20px" }}>
-    <h2 style={{ marginBottom: "20px", color: "#7ebeab" }}>スキル</h2>
-    <table
+const App = () => {
+  return (
+    <div
       style={{
-        width: "100%",
-        borderCollapse: "collapse",
-        marginBottom: "20px",
+        margin: 0,
+        padding: 0,
+        fontFamily: "Arial, sans-serif",
+        boxSizing: "border-box",
+        backgroundColor: "#08081a", // 背景色を #08081a に
+        color: "#a0d8ef", // 文字色を #a0d8ef に
       }}
     >
-      <thead>
-        <tr>
-          <th
+      {/* CSSリセットを適用 */}
+      <style>
+        {
+          html, body {
+            margin: 0;
+            padding: 0;
+          }
+          * {
+            box-sizing: border-box;
+          }
+        }
+      </style>
+
+      <Header /> {/* ヘッダーコンポーネント */}
+      <main
+        style={{
+          paddingTop: "70px", // ヘッダー分のスペースを確保
+          textAlign: "center",
+          minHeight: "100vh",
+        }}
+      >
+        <TextContent /> {/* 文字と文章を表示するコンポーネント */}
+        
+        {/* 画像の表示 */}
+        <div style={{ marginTop: "20px" }}>
+          <img
+            src="https://raw.githubusercontent.com/HibikiKobayashii/HibikiKobayashii.github.io/main/gazo/hibiki.jpg"
+            alt="小林響"
             style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              backgroundColor: "#000000",
-              color: "#7ebeab",
+              width: "300px",
+              height: "auto",
+              borderRadius: "10px",
+              border: "1px solid #a0d8ef", // ボーダー色を文字色に合わせる
             }}
-          >
-            技術
-          </th>
-          <th
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              backgroundColor: "#000000",
-              color: "#7ebeab",
-            }}
-          >
-            レベル
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            HTML
-          </td>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            初心者
-          </td>
-        </tr>
-        <tr>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            CSS
-          </td>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            初心者
-          </td>
-        </tr>
-        <tr>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            JavaScript
-          </td>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            中級者
-          </td>
-        </tr>
-        <tr>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            React
-          </td>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            使い始めた
-          </td>
-        </tr>
-        <tr>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            Next.js
-          </td>
-          <td
-            style={{
-              padding: "10px",
-              border: "1px solid #7ebeab",
-              color: "#7ebeab",
-            }}
-          >
-            使い始めた
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</section>
+          />
+        </div>
+
+        {/* スキルセクション */}
+        <section
+          id="skills"
+          style={{
+            marginTop: "50px",
+            padding: "20px",
+            backgroundColor: "#03090d",
+            borderRadius: "10px",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ flex: 1, paddingRight: "20px" }}>
+            <h2 style={{ marginBottom: "20px", color: "#7ebeab" }}>スキル</h2>
+            <table
+              style={{
+                width: "100%",
+                borderCollapse: "collapse",
+                marginBottom: "20px",
+              }}
+            >
+              <thead>
+                <tr>
+                  <th
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      backgroundColor: "#000000",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    技術
+                  </th>
+                  <th
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      backgroundColor: "#000000",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    レベル
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    HTML
+                  </td>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    初心者
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    CSS
+                  </td>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    初心者
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    JavaScript
+                  </td>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    中級者
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    React
+                  </td>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    使い始めた
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    Next.js
+                  </td>
+                  <td
+                    style={{
+                      padding: "10px",
+                      border: "1px solid #7ebeab",
+                      color: "#7ebeab",
+                    }}
+                  >
+                    使い始めた
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <SkillDescription /> {/* 右側文字のコンポーネント */}
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default App;
