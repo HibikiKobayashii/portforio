0aaf7fb66cc39e6bd35e940c0b0b925c0e8562e4
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-08 23:50:30 +0900

    Create TextContent.tsx

---
+++ app/components/TextContent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const TextContent = () => {
+  return (
+    <div>
+      <h1 style={{ margin: "20px 0", color: "#a0d8ef" }}>I am an IoT Student !!</h1>
+      <p style={{ fontSize: "18px", color: "#a0d8ef" }}>
+        開志専門職大学でIoTを学んでいる小林響と申します！ <br />
+        XR系に力を入れています！
+      </p>
+    </div>
+  );
+};
+
+export default TextContent;
