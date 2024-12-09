ed3143dfa24a03d0b822f68b3548285dfee03ac4
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-09 11:31:35 +0900

    Update SkillDescription.tsx

--- app/components/SkillDescription.tsx
+++ app/components/SkillDescription.tsx
@@ -5,7 +5,7 @@ const SkillDescription = () => {
     <div style={{ flex: 1 }}>
 
      
-      <p style={{ fontSize: "16px", lineHeight: "1.6", color: "#7ebeab" }}>
+      <p style={{ fontSize: "16px", lineHeight: "1.6", color: "#a0d8ef" }}>
         ここでは、現在私が使える技術とそのレベルを紹介しています。
          HTMLとCSSは授業で習った程度です。主にJavaScriptを使用しています。ReactとNext.jsは使い始めたばかりですが、インターンによる実際のプロジェクトで学びながら開発を行いました。このサイトでも使用しています。
       </p>
