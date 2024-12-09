104596816d880f745b22c9b7cac7d6079ebac97e
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-09 11:04:40 +0900

    Update SkillDescription.tsx

--- app/components/SkillDescription.tsx
+++ app/components/SkillDescription.tsx
@@ -6,9 +6,8 @@ const SkillDescription = () => {
 
       <h2 style={{ marginBottom: "20px", color: "#7ebeab" }}>スキルの説明</h2>
       <p style={{ fontSize: "16px", lineHeight: "1.6", color: "#7ebeab" }}>
-        ここでは、現在私が使える技術とそのレベルを紹介しています。HTMLとCSSは初心者レベルで、
-        JavaScriptは中級者として扱えます。ReactとNext.jsは使い始めたばかりですが、
-        実際のプロジェクトで学びながら進めています。
+        ここでは、現在私が使える技術とそのレベルを紹介しています。
+        <br> HTMLとCSSは授業で習った程度です。主にJavaScriptを使用しています。ReactとNext.jsは使い始めたばかりですが、インターンによる実際のプロジェクトで学び始め、このサイトでも使用しています。
       </p>
     </div>
   );
