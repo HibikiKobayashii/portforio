a57337e9bd14fee56e77513b7d0db10cdea8aa41
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-16 15:28:32 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export"     // 静的サイトとしてエクスポートする
-     // URL末尾にスラッシュを追加する
-  // 他の設定も必要に応じて追加できます
-};
-
-module.exports = {
+  output: "export", 
   images: {
     remotePatterns: [
       {
@@ -15,6 +10,9 @@ module.exports = {
         pathname: "/app/components/gazo/hibiki.jpg",
       },
     ],
+  },
+  env: {
+    SHEETDB_ADDRESS: 'https://sheetdb.io/api/v1/pgotw150b2ot3',
   },
 };
 
