863847d8f8c2267f8ad681495d14aaf6ea68ccee
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-17 13:40:21 +0900

    Update sheetdb.js

--- pages/lib/sheetdb.js
+++ pages/lib/sheetdb.js
@@ -1,7 +1,7 @@
-import { sheetDB } from 'sheetdb-node';
+const { sheetDB } = require('sheetdb-node');
 
 const client = sheetDB({
   address: process.env.SHEETDB_ADDRESS,
 });
 
-export default client;
+module.exports = client;
