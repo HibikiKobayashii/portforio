dc1538056acf7839e8baf731af77c7244a221bec
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-17 14:16:54 +0900

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -14,7 +14,7 @@ const Contact = () => {
     };
 
     try {
-      const response = await fetch("../pages/api/DB.js", {
+      const response = await fetch("/api/DB", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
