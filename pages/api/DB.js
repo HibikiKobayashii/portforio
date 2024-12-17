2c496f6338a5e3d854eb21888763a141a76ecc29
HibikiKobayashii <168159927+HibikiKobayashii@users.noreply.github.com>
2024-12-17 13:41:48 +0900

    Update DB.js

--- pages/api/DB.js
+++ pages/api/DB.js
@@ -1,21 +1,14 @@
-// pages/api/saveToSheet.js
-
-import { sheetDB } from 'sheetdb-node';
-
-const client = sheetDB({
-  address: process.env.SHEETDB_ADDRESS,
-});
+const client = require('../pages/lib/sheetdb.js'); // 修正済み
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const { name, email, message } = req.body; 
+      const { name, email, message } = req.body;
 
-      // SheetDB に挿入するデータを整形
       const data = {
-        B: name,  // B列に名前
-        D: email, // D列にメールアドレス
-        E: message // E列に内容
+        B: name,    // B列に名前
+        D: email,   // D列にメールアドレス
+        E: message  // E列に内容
       };
 
       await client.create(data);
